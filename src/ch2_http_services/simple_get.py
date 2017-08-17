774d4ce13a1cef45fc8bc6c6bce42b2a12aec670
benlove <benuklove@gmail.com>
2017-08-16 23:48:28 -0500

    initial commit

---
+++ src/ch2_http_services/simple_get.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def main():
+    url = 'https://talkpython.fm/'
+
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        print("Error requesting URL, {}".format(resp.status_code))
+        return
+
+    print(resp.text[:500])
+    print(resp.status_code)
+
+if __name__ == '__main__':
+    main()
